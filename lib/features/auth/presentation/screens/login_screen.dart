c7ee3f497c6b88badebee6bd3c481b0e3cde912c
Raj singh <singhraj092932@gmail.com>
2026-06-30 20:22:19 +0530

    connected firebase with ui

--- lib/features/auth/presentation/screens/login_screen.dart
+++ lib/features/auth/presentation/screens/login_screen.dart
@@ -1,15 +1,52 @@
 import 'package:challan_app/core/theme/app_theme.dart';
+import 'package:challan_app/features/auth/presentation/screens/registration.dart';
+import 'package:challan_app/features/challan/presntation/screens/main_screen.dart';
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
+import 'package:google_sign_in/google_sign_in.dart';
 
-class LoginScreen extends StatelessWidget {
+class LoginScreen extends StatefulWidget {
   const LoginScreen({super.key});
+
+  @override
+  State<LoginScreen> createState() => _LoginScreenState();
+}
+
+class _LoginScreenState extends State<LoginScreen> {
+  TextEditingController email = TextEditingController();
+  TextEditingController password = TextEditingController();
+  bool _obscure = false;
+  signIn() async {
+    try {
+      await FirebaseAuth.instance.signInWithEmailAndPassword(
+        email: email.text.trim(),
+        password: password.text.trim(),
+      );
+    } on FirebaseAuthException catch (e) {
+      ScaffoldMessenger.of(
+        context,
+      ).showSnackBar(SnackBar(content: Text('Login Failed $e')));
+      print(e);
+    }
+  }
+
+  Future<void> signInWithGoogle() async {
+    final googleUser = await GoogleSignIn().signIn();
+    if (googleUser == null) return;
+    final googleAuth = await googleUser.authentication;
+    final credential = GoogleAuthProvider.credential(
+      accessToken: googleAuth.accessToken,
+      idToken: googleAuth.idToken,
+    );
+    await FirebaseAuth.instance.signInWithCredential(credential);
+  }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Padding(
-        padding: const EdgeInsets.symmetric(vertical: 30, horizontal: 20),
-        child: SingleChildScrollView(
+      body: SingleChildScrollView(
+        child: Padding(
+          padding: const EdgeInsets.symmetric(vertical: 30, horizontal: 10),
           child: Column(
             crossAxisAlignment: CrossAxisAlignment.center,
             children: [
@@ -29,8 +66,12 @@ class LoginScreen extends StatelessWidget {
                   color: Color(0xffF5E6C8),
                   borderRadius: BorderRadius.circular(30),
                 ),
-          
-                child: Icon(Icons.menu_book, size: 70, color: AppColors.primary),
+
+                child: Icon(
+                  Icons.menu_book,
+                  size: 70,
+                  color: AppColors.primary,
+                ),
               ),
               SizedBox(height: 5),
               Text(
@@ -43,7 +84,7 @@ class LoginScreen extends StatelessWidget {
               ),
               SizedBox(height: 20),
               Padding(
-                padding: const EdgeInsets.all(10.0),
+                padding: const EdgeInsets.all(5.0),
                 child: Column(
                   children: [
                     Row(
@@ -57,17 +98,19 @@ class LoginScreen extends StatelessWidget {
                         borderRadius: BorderRadius.circular(20),
                       ),
                       child: TextField(
-                        
                         decoration: InputDecoration(
                           hintText: 'Email',
                           hintStyle: TextStyle(color: AppColors.grey),
                           contentPadding: EdgeInsets.all(25),
                           border: InputBorder.none,
-                          prefixIcon: Icon(Icons.email, color: AppColors.primary),
+                          prefixIcon: Icon(
+                            Icons.email,
+                            color: AppColors.primary,
+                          ),
                         ),
                       ),
                     ),
-          
+
                     SizedBox(height: 30),
                     Row(
                       crossAxisAlignment: CrossAxisAlignment.start,
@@ -82,12 +125,29 @@ class LoginScreen extends StatelessWidget {
                         borderRadius: BorderRadius.circular(20),
                       ),
                       child: TextField(
+                        obscureText: _obscure,
                         decoration: InputDecoration(
                           hintText: 'Password',
                           hintStyle: TextStyle(color: AppColors.grey),
                           contentPadding: EdgeInsets.all(25),
                           border: InputBorder.none,
-                          prefixIcon: Icon(Icons.lock, color: AppColors.primary),
+                          prefixIcon: Icon(
+                            Icons.lock,
+                            color: AppColors.primary,
+                          ),
+                          suffixIcon: GestureDetector(
+                            onTap: () {
+                              setState(() {
+                                _obscure = !_obscure;
+                              });
+                            },
+                            child: Icon(
+                              _obscure
+                                  ? Icons.visibility
+                                  : Icons.visibility_off,
+                              color: AppColors.primary,
+                            ),
+                          ),
                         ),
                       ),
                     ),
@@ -107,7 +167,9 @@ class LoginScreen extends StatelessWidget {
                         fixedSize: Size(360, 60),
                         backgroundColor: AppColors.primary,
                       ),
-                      onPressed: () {},
+                      onPressed: () {
+                        signIn();
+                      },
                       child: Text(
                         'Sign in',
                         style: TextStyle(color: Colors.white, fontSize: 23),
@@ -133,7 +195,9 @@ class LoginScreen extends StatelessWidget {
                     ),
                     SizedBox(height: 20),
                     OutlinedButton(
-                      onPressed: () {},
+                      onPressed: () {
+                        signInWithGoogle();
+                      },
                       style: OutlinedButton.styleFrom(
                         fixedSize: Size(100, 55),
                         side: BorderSide(color: AppColors.grey),
@@ -152,16 +216,24 @@ class LoginScreen extends StatelessWidget {
                     ),
                     SizedBox(height: 20),
                     Row(
-                     mainAxisAlignment: MainAxisAlignment.center,
+                      mainAxisAlignment: MainAxisAlignment.center,
                       children: [
                         Text(
                           "Don't have an account?",
                           style: TextStyle(color: AppColors.grey),
                         ),
-                        SizedBox(width: 7,),
-                        Text(
-                          'Create Account',
-                          style: TextStyle(color: AppColors.primary),
+                        SizedBox(width: 7),
+                        GestureDetector(
+                          onTap: () {
+                            Navigator.push(
+                              context,
+                              MaterialPageRoute(builder: (_) => Registration()),
+                            );
+                          },
+                          child: Text(
+                            'Create Account',
+                            style: TextStyle(color: AppColors.primary),
+                          ),
                         ),
                       ],
                     ),
--- lib/features/challan/presntation/screens/main_screen.dart
+++ lib/features/challan/presntation/screens/main_screen.dart
@@ -1,13 +1,35 @@
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
+import 'package:google_sign_in/google_sign_in.dart';
 
-class MainScreen extends StatelessWidget {
+class MainScreen extends StatefulWidget {
   const MainScreen({super.key});
+
+  @override
+  State<MainScreen> createState() => _MainScreenState();
+}
+
+class _MainScreenState extends State<MainScreen> {
+  signout() async {
+    await FirebaseAuth.instance.signOut();
+    await GoogleSignIn().signOut();
+  }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       body: Center(
-        child: Text('Home'),
+        child: Column(
+          children: [
+            Text('Home'),
+            ElevatedButton(
+              onPressed: () {
+                signout();
+              },
+              child: Text('Sign Out'),
+            ),
+          ],
+        ),
       ),
     );
   }
